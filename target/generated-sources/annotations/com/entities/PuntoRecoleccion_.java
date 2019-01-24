9174ff9757f380decdd09080384ee3ae703e5e84
danny9mvp <danny9mvp@gmail.com>
2019-01-24 10:53:53 -0500

    Removidos facets de CDI y JSF

--- target/generated-sources/annotations/com/entities/PuntoRecoleccion_.java
+++ target/generated-sources/annotations/com/entities/PuntoRecoleccion_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-18T12:04:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-24T10:52:57")
 @StaticMetamodel(PuntoRecoleccion.class)
 public class PuntoRecoleccion_ { 
 
